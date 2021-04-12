8ad192a1566b337007c2b36c7e551f3c893258ba
SolitaryOrz <solitaryorz@outlook.com>
2021-04-12 15:57:30 +0800

    user pages

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,66 +1,91 @@
-import Taro from '@tarojs/taro'
 import React, { Component } from 'react'
-import { View, Text, Button } from '@tarojs/components'
+import { View, Text, Image, Navigator } from '@tarojs/components'
+import { request } from '@utils/request'
+
+import TabBar from '@components/topTabBar/topTabBar'
+import Slider from './slider/slider'
+import Nav from './nav/nav'
+import Nav3 from './nav3/nav3'
 import './index.less'
 
 export default class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newGoodsList: [],
+      goodsPlace: []
+    }
+  }
 
-  componentDidMount() { }
+  componentWillMount() {
+    this.getNewGoodsList();
+    this.getGoodsPlace();
+  }
 
-  componentWillUnmount() { }
-
-  componentDidShow() { }
-
-  componentDidHide() { }
-
-  handleDownload(){
-    const fileName = 'test_20200808225919_348.pdf';
-    let $this = this;
-    Taro.downloadFile({
-      url: 'http://106.52.218.85:8000/media/goods/files/test_20200808225919_348.pdf',
-      success: res => {
-        if(res.statusCode === 200){
-          var filePath = res.tempFilePath;
-          // let manager = Taro.getFileSystemManager();
-          // // 判断目录是否存在
-          // manager.access({
-          //   path: `${Taro.env.USER_DATA_PATH}/download`,
-          //   success: res => {
-          //     // 保存文件前查看是否存在该文件
-          //     manager.access({
-          //       path: `${Taro.env.USER_DATA_PATH}/download/${fileName}`,
-          //       success: res => {
-          //         return false;
-          //       },
-          //       fail: err => {
-          //         manager.saveFile({
-          //           tempFilePath: filePath, // filePath为保存到本地的临时路径
-          //           filePath: `${Taro.env().USER_DATA_PATH}/download/${fileName}`,
-          //           success: res => {
-                      
-          //           }
-          //         })
-          //       }
-          //     })
-          //   }
-          // })
-          Taro.openDocument({
-            filePath: filePath,
-            showMenu: true,
-            success: res => {
-              console.log('成功')
-            }
-          })
-        }
+  // 获取最新活动列表
+  async getNewGoodsList() {
+    const data = await request({
+      url: '/newGoodsListView'
+    })
+    let newGoodsList = data.results;
+    newGoodsList.forEach((item)=>{
+      if(item.goods_front_image) {
+        item.goods_front_image = item.goods_front_image.replace(/http:\/\/127.0.0.1/, 'https://focus.fmg.net.cn');
       }
     })
+    this.setState({
+      newGoodsList
+    })
+    console.log('newGoodsList',newGoodsList)
+  }
+
+  // 获取活动场地列表
+  async getGoodsPlace() {
+    const data = await request({
+      url: '/goodsPlaceListView'
+    })
+    let goodsPlace = data.results
+    goodsPlace.forEach((item)=>{
+      item.goods_front_image = item.goods_front_image.replace(/http:\/\/127.0.0.1/, 'https://focus.fmg.net.cn');
+    })
+    this.setState({
+      goodsPlace
+    })
+    console.log('goodsPlace',goodsPlace)
   }
 
   render() {
+    const {newGoodsList, goodsPlace} = this.state;
     return (
-      <View className='user'>
-        <Text>Hello world!</Text>
-        <Button onClick={this.handleDownload} >下载</Button>
+      <View className='index'>
+        <TabBar></TabBar>
+        <Slider />
+        <Nav />
+        <Nav3 />
+        <View className='container'>
+          <Text className='container_title'>最新活动</Text>
+          <View className='activity_list'>
+            {newGoodsList.map((item)=>(
+              <Navigator className='activity' url={'/pages/detail/index?id='+item.id} key={item.id}>
+                <Image src={item.goods_front_image}></Image>
+                <Text className='title'>{item.name}</Text>
+              </Navigator>
+            ))}
+          </View>
+        </View>
+        <View className='container'>
+          <Text className='container_title'>活动场地</Text>
+          <View className='site_wrap'>
+            {goodsPlace.map((item,index)=>(
+              <Navigator url={'/pages/detail/index?id='+item.id} key={item.id}>
+                {index <= 1
+                  ? <Image src={item.goods_front_image} className='site_big' />
+                  : <Image src={item.goods_front_image} className='site_small' />
+                }
+              </Navigator>
+            ))}
+          </View>
+        </View>
       </View>
     )
   }
